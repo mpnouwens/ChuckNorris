354d525ca43dc994f4c8b02d93fdc1ef7538b0b9
Michael Nouwens <nouwensmichael0@gmail.com>
2019-11-27 18:48:20 +0200

    Final fix

--- client/src/App.js
+++ client/src/App.js
@@ -6,11 +6,12 @@ import logo from "./logo.png";
 import Categories from "./components/Categories";
 import JokeCategory from "./components/JokeCategory";
 import RandomJoke from "./components/RandomJoke";
+import SearchJoke from "./components/SearchJoke";
 import { Link } from "react-router-dom";
 
 //Todo: Add proxy
 const client = new ApolloClient({
-  uri: "https://ihcxe.sse.codesandbox.io/graphql"
+  uri: "https://ddw9o.sse.codesandbox.io/graphql"
 });
 
 class App extends Component {
@@ -25,7 +26,32 @@ class App extends Component {
               style={{ width: 150, display: "block", margin: "auto" }}
             />
           </div>
+          <div className="row justify-content-center m-3">
+            <div className="col-12 col-md-10 col-lg-8">
+              <div className="card card-sm">
+                <div className="card-body row no-gutters align-items-center">
+                  <div className="col">
+                    <input
+                      placeholder="Search"
+                      type="Search"
+                      className="form-control form-control-lg form-control-borderless"
+                    />
+                  </div>
+                  <div className="col-auto">
+                    <Link
+                      to={`/searchjoke`}
+                      className="btn btn-success"
+                      style={{ width: 150, marginLeft: 10 }}
+                    >
+                      Search
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
           <Route exact path="/" component={Categories} />
+          <Route exact path="/search" component={SearchJoke} />
           <Route
             exact
             path="/categoryjoke/:categories"
